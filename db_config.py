924958f3b1e1635426740c6b8e8d4ee0a133eeee
Rongbin <mikeliu@gmai.com>
2026-06-24 17:38:37 +1200

    add db_config.py file

---
+++ db_config.py
@@ -0,0 +1,11 @@
+# db_config.py
+
+DB_CONFIG = {
+    'host': "46.250.243.80",
+    'port': 3306,            
+    'user': "intern",  
+    'password': "spiritx2026",
+    'database': "Jobi"
+}
+
+GEOCODING_API="AIzaSyDPNkF-nF_gKavcmYtCQZfs6zHx6K8avf0"  # Google API
